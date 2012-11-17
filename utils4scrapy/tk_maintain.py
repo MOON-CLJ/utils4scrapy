5b3c485214d91f48bbcbc79773d0f1f391bba479
MOON_CLJ <lijunli2598@gmail.com>
2012-11-17 23:22:22 +0800

    remove class TokenMaintain

--- utils4scrapy/tk_maintain.py
+++ utils4scrapy/tk_maintain.py
@@ -10,76 +10,82 @@ import socket
 LIMIT_URL = 'https://api.weibo.com/2/account/rate_limit_status.json?access_token={access_token}'
 EXPIRED_TOKEN = 21327
 INVALID_ACCESS_TOKEN = 21332
+HOURS_LIMIT = 1000
 
 
-class TkMaintain(object):
-    @staticmethod
-    def token_status(token):
-        retry = 0
-        while 1:
-            retry += 1
-            if retry > 3:
-                raise CloseSpider('CHECK LIMIT STATUS FAIL')
+def token_status(token):
+    retry = 0
+    while 1:
+        retry += 1
+        if retry > 3:
+            raise CloseSpider('CHECK LIMIT STATUS FAIL')
 
-            try:
-                log.msg("[Token Status] token: {token}, sleep one second, wait to check".format(token=token))
-                time.sleep(1)
-                log.msg("[Token Status] now check")
+        try:
+            log.msg("[Token Status] token: {token}, sleep one second, wait to check".format(token=token))
+            time.sleep(1)
+            log.msg("[Token Status] now check")
 
-                http = urllib3.PoolManager()
-                resp = http.request('GET', LIMIT_URL.format(access_token=token))
-                resp = json.loads(resp.data)
+            http = urllib3.PoolManager()
+            resp = http.request('GET', LIMIT_URL.format(access_token=token))
+            resp = json.loads(resp.data)
 
-                if 'error' in resp:
-                    if resp['error'] == 'expired_token':
-                        return EXPIRED_TOKEN
-                    if resp['error'] == 'invalid_access_token':
-                        return INVALID_ACCESS_TOKEN
+            if 'error' in resp:
+                if resp['error'] == 'expired_token':
+                    return EXPIRED_TOKEN
+                elif resp['error'] == 'invalid_access_token':
+                    return INVALID_ACCESS_TOKEN
+                else:
+                    raise CloseSpider('UNKNOWN TOKEN STATUS ERROR')
 
-                reset_time_in = resp['reset_time_in_seconds']
-                remaining = resp['remaining_user_hits']
-                return reset_time_in, remaining
-            except socket.gaierror:
-                pass
+            reset_time_in = resp['reset_time_in_seconds']
+            remaining = resp['remaining_user_hits']
+            return reset_time_in, remaining
+        except socket.gaierror:
+            pass
 
-    @staticmethod
-    def maintain(r, mongo, api_key, req_count, tk_alive, at_least=1, hourly=False):
-        log.msg('[Token Maintain] begin maintain')
 
-        # 从应用导入所有未过期的token，并初始使用次数为0，相应的alive为True
-        for user in mongo.users.find():
-            if user['expires_in'] > time.time():
-                req_count.set(user['access_token'], 0)
-                tk_alive.hset(user['access_token'], user['expires_in'])
+def maintain(r, mongo, api_key, req_count, tk_alive, at_least=1, hourly=False):
+    log.msg('[Token Maintain] begin maintain')
 
-        tokens_in_redis = req_count.all_tokens()
-        print 'before alive:', len(tokens_in_redis)  # 清理之前
-        alive_count = 0
-        for token in tokens_in_redis:
-            if tk_alive.isalive(token):
-                alive_count += 1
-            else:
-                req_count.delete(token)
-                tk_alive.drop_tk(token)
+    # 从应用导入所有未过期的token，并初始使用次数为0，相应的alive为True
+    for user in mongo.users.find():
+        if user['expires_in'] > time.time():
+            req_count.set(user['access_token'], 0)
+            tk_alive.hset(user['access_token'], user['expires_in'])
 
-        tokens_in_redis = req_count.all_tokens()
-        print 'after alive:', len(tokens_in_redis)  # 清理之后
+    tokens_in_redis = req_count.all_tokens()
+    print 'before alive:', len(tokens_in_redis)  # 清理之前
+    alive_count = 0
+    for token in tokens_in_redis:
+        if tk_alive.isalive(token):
+            alive_count += 1
+        else:
+            req_count.delete(token)
+            tk_alive.drop_tk(token)
 
-        if alive_count < at_least:
-            raise CloseSpider('TOKENS COUNT NOT REACH AT_LEAST')
+    tokens_in_redis = req_count.all_tokens()
+    print 'after alive:', len(tokens_in_redis)  # 清理之后
 
-    @classmethod
-    def update_used(cls, req_count):
-        log.msg("[Token Maintain] begin update_used")
-        tokens_inqueue = req_count.all_tokens()
-        for token in tokens_inqueue:
-            token_status = cls.token_status(token)
-            if token_status in [EXPIRED_TOKEN, INVALID_ACCESS_TOKEN]:
-                req_count.delete(token)
-                continue
+    if alive_count < at_least:
+        raise CloseSpider('TOKENS COUNT NOT REACH AT_LEAST')
 
-            _, remaining = token_status
-            req_count.reset(token, 1000 - remaining)
+    log.msg('[Token Maintain] end maintain')
+
+
+def calibration(req_count, tk_alive):
+    log.msg('[Token Maintain] begin calibration')
+    tokens_in_redis = req_count.all_tokens()
+    for token in tokens_in_redis:
+        tk_status = token_status(token)
+        if tk_status in [EXPIRED_TOKEN, INVALID_ACCESS_TOKEN]:
+            req_count.delete(token)
+            tk_alive.drop_tk(token)
+            continue
+
+        _, remaining = tk_status
+        req_count.reset(token, HOURS_LIMIT - remaining)
+
+    log.msg('[Token Maintain] end calibration')
 
 
 if __name__ == '__main__':
@@ -119,8 +125,8 @@ if __name__ == '__main__':
         req_count = ReqCount(r, api_key)
         tk_alive = TkAlive(r, api_key)
 
-        at_least = 10
+        at_least = 6
 
         logbk.info('maintain begin')
-        TkMaintain.maintain(r, db, api_key, req_count, tk_alive, at_least=at_least, hourly=True)
+        maintain(r, db, api_key, req_count, tk_alive, at_least=at_least, hourly=True)
         logbk.info('maintain end')
