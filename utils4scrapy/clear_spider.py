e2de91d9f2dc2ee2048e87d9622dc4e7d720c69a
MOON_CLJ <lijunli2598@gmail.com>
2013-02-05 18:53:06 +0800

    加入查看schedule的状态

--- utils4scrapy/clear_spider.py
+++ utils4scrapy/clear_spider.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# usage: py clear_spider.py public_timeline [st]
 
 import sys
 from tk_maintain import _default_redis
@@ -16,11 +17,19 @@ REDIS_PORT = 6379
 QUEUE_KEY = '%(spider)s:requests'
 DUPEFILTER_KEY = '%(spider)s:dupefilter'
 
-
 spider_name = sys.argv[1]
+not_del = False
+if len(sys.argv) > 2 and sys.argv[2] == 'st':
+    not_del = True
+
 host = REDIS_HOST
 port = REDIS_PORT
 r = _default_redis(host, port)
-r.delete(QUEUE_KEY % {'spider': spider_name})
-r.delete(DUPEFILTER_KEY % {'spider': spider_name})
-print 'clear %s' % spider_name
+
+print 'scheduled requests: %s' % r.zcard(DUPEFILTER_KEY % {'spider': spider_name})
+print 'dupefiler requests: %s' % r.scard(DUPEFILTER_KEY % {'spider': spider_name})
+
+if not not_del:
+    r.delete(QUEUE_KEY % {'spider': spider_name})
+    r.delete(DUPEFILTER_KEY % {'spider': spider_name})
+    print 'clear %s' % spider_name
