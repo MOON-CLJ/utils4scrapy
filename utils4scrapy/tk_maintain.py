40738132f2ec17d688c38dde9b03c99687b9f925
MOON_CLJ <lijunli2598@gmail.com>
2013-04-04 17:37:18 +0800

    tk

--- utils4scrapy/tk_maintain.py
+++ utils4scrapy/tk_maintain.py
@@ -20,9 +20,10 @@ MONGOD_HOST = 'localhost'
 MONGOD_PORT = 27017
 # prod
 API_KEY = '4131380600'
+"""
 # dev
 API_KEY = '1966311272'
-
+"""
 
 def _default_mongo(host=MONGOD_HOST, port=MONGOD_PORT, usedb='simple'):
     # 强制写journal，并强制safe
